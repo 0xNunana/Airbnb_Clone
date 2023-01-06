bcdb99c8abc1e462923234c8c44fb6592a7a0359
0xNunana <kudayapaul@gmail.com>
2023-01-06 14:46:05 +0000

    fixed escaped character

--- xing-lodge/components/Banner.js
+++ xing-lodge/components/Banner.js
@@ -3,17 +3,17 @@ import React from 'react'
 
 function Banner() {
   return (
-    <div className='relative h-[300px] sm:h-[400px] lg:h-[500px] xl:h-[600px] 2xl:h-[700]'>
-        <Image src="https://images.pexels.com/photos/1268855/pexels-photo-1268855.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt='banner' fill/>
+    <div className="relative h-[300px] sm:h-[400px] lg:h-[500px] xl:h-[600px] 2xl:h-[700]">
+        <Image src="https://images.pexels.com/photos/1268855/pexels-photo-1268855.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="banner" fill/>
     <div className="absolute top-1/2 w-full text-center">
-        <p className='text-sm sm:text-lg'>
+        <p className="text-sm sm:text-lg">
             Not Sure where to go?
         </p>
-        <button className='text-purple-500 bg-white px-10 py-4 shadow-md rounded-full dont-bold my-3 hover:shadow-xl active:scale-90 transition duration-150'>I'm Flexible</button>
+        <button className="text-purple-500 bg-white px-10 py-4 shadow-md rounded-full font-bold my-3 hover:shadow-xl active:scale-90 transition duration-150"><h2>I'm Flexible</h2></button>
     </div>
     
     </div>
   )
 }
 
-export default Banner
+export default Banner;
